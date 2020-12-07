02b39a3f9221223484f3373f4eee6ef86196bd2c
Gabriel González <ggfresard@gmail.com>
2020-12-07 15:24:10 -0300

    t

--- rebaaccounting/settings.py
+++ rebaaccounting/settings.py
@@ -154,5 +154,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'front/static/front')
+]
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
