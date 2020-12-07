69e9a4b7efe16bb00ab4bfecaddecbae3d2f3d0c
Le Gab <ggfresard@gmail.com>
2020-12-07 14:47:40 -0300

    initial

--- rebaaccounting/settings.py
+++ rebaaccounting/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '#17@yelq5x-r=!g=i#!jr(zk5!usp#l+fr$**ouo0q4w%1u=m^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['re-ba-accounting-dev.herokuapp.com']
 
 
 # Application definition
@@ -154,3 +154,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
